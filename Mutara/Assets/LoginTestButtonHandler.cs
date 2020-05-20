27df8af0565f3303f7972cc4c27c638d42c0b479
Tony Rasa <trasa@meancat.com>
2020-05-20 11:36:35 -0700

    initial commit

---
+++ Mutara/Assets/LoginTestButtonHandler.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class LoginTestButtonHandler : MonoBehaviour
+{
+    public void OnClick()
+    {
+
+        EditorUtility.DisplayDialog("Clicked", "You clicked!", "Great");
+    }
+}
